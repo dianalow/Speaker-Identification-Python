4792ecd013feab917a5bf1ccd4b4653d9f8fc83c
Diana Low <lowdiana@gmail.com>
2020-03-26 10:02:37 +0800

    modified code to be more flexible

--- modeltraining.py
+++ modeltraining.py
@@ -3,51 +3,55 @@ import numpy as np
 from scipy.io.wavfile import read
 from sklearn import mixture
 from featureextraction import extract_features
+import os
+
 #from speakerfeatures import extract_features
 import warnings
 warnings.filterwarnings("ignore")
 
 #path to training data
 # source   = "development_set/"
-source   = "trainingData/"
+source   = "/media/sf_D_DRIVE/voxforge_16_16/training/"
 
 #path where training speakers will be saved
 
 # dest = "speaker_models/"
 # train_file = "development_set_enroll.txt"
 
-dest = "Speakers_models/"
-train_file = "trainingDataPath.txt"
-file_paths = open(train_file,'r')
+dest = "speaker_models/"
 
 count = 1
 # Extracting features for each speaker (5 files per speakers)
+speakers = [ name for name in os.listdir(source) if os.path.isdir(os.path.join(source, name)) ]
+print(len(speakers),"speakers identified.")
+print(speakers)
+
 features = np.asarray(())
-for path in file_paths:
-    path = path.strip()
-    print (path)
 
-    # read the audio
-    sr,audio = read(source + path)
+for speaker in speakers:
+    files = [f for f in os.listdir(os.path.join(source, speaker)) if os.path.isfile(os.path.join(source, speaker, f)) & f.endswith(".wav") ]
+    print(files)
+    total_samples = len(files)
+    for n, file in enumerate(files):
+        # read the audio
+        sr,audio = read(os.path.join(source, speaker, file))
 
-    # extract 40 dimensional MFCC & delta MFCC features
-    vector   = extract_features(audio,sr)
+        # extract 40 dimensional MFCC & delta MFCC features
+        vector   = extract_features(audio,sr)
 
-    if features.size == 0:
-        features = vector
-    else:
-        features = np.vstack((features, vector))
-    # when features of 5 files of speaker are concatenated, then do model training
-	# -> if count == 5: --> edited below
-    if count == 1:
-        gmm = mixture.GaussianMixture(n_components = 1, covariance_type='diag',n_init = 3)
-        gmm.fit(features)
+        if features.size == 0:
+            features = vector
+        else:
+            features = np.vstack((features, vector))
+        # when features of 5 files of speaker are concatenated, then do model training
+    	# -> if count == 5: --> edited below
+        if n == total_samples-1:
+            gmm = mixture.GaussianMixture(n_components = total_samples, covariance_type='diag')
+            gmm.fit(features)
 
-        # dumping the trained gaussian model
-        picklefile = path.split("-")[0]+".gmm"
+            # dumping the trained gaussian model
+            picklefile = speaker.split("-")[0]+".gmm"
 
-        pickle.dump(gmm,open(dest + picklefile,'wb'))
-        print('+ modeling completed for speaker:',picklefile," with data point = ",features.shape)
-        features = np.asarray(())
-        count = 0
-    count = count + 1
+            pickle.dump(gmm,open(dest + picklefile,'wb'))
+            print('+ modeling completed for speaker:',picklefile," with data point = ",features.shape)
+            features = np.asarray(())
--- test.py
+++ test.py
@@ -7,19 +7,13 @@ from featureextraction import extract_features
 import warnings
 warnings.filterwarnings("ignore")
 import time
+import math
 
-"""
-#path to training data
-source   = "development_set/"
-modelpath = "speaker_models/"
-test_file = "development_set_test.txt"
-file_paths = open(test_file,'r')
-"""
 #path to training data
 source   = "SampleData/"
 
 #path where training speakers will be saved
-modelpath = "Speakers_models/"
+modelpath = "speaker_models/"
 
 gmm_files = [os.path.join(modelpath,fname) for fname in
               os.listdir(modelpath) if fname.endswith('.gmm')]
@@ -31,14 +25,13 @@ speakers = [fname.split("/")[-1].split(".gmm")[0] for fname in gmm_files]
 error = 0
 total_sample = 0.0
 
-
 print ("Do you want to Test a Single Audio: Press '1' or The complete Test Audio Sample: Press '0' ?")
 take = int(input().strip())
 if take == 1:
     print ("Enter the File name from Test Audio Sample Collection :")
     path = input().strip()
     print ("Testing Audio : ", path)
-    sr,audio = read(source + path)
+    sr,audio = read(path)
     vector   = extract_features(audio,sr)
     log_likelihood = np.zeros(len(models))
 
@@ -52,38 +45,37 @@ if take == 1:
 
     time.sleep(1.0)
 elif take == 0:
-    test_file = "testSamplePath.txt"
-    file_paths = open(test_file,'r')
+    print ("Enter the path to samples:")
+    path = input().strip()
 
+    speakers = [ name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name)) ]
 
-    # Read the test directory and get the list of test audio files
-    for path in file_paths:
+    for speaker in speakers:
+        files = [f for f in os.listdir(os.path.join(path, speaker)) if os.path.isfile(os.path.join(path, speaker, f)) & f.endswith(".wav") ]
 
-            total_sample += 1.0
-            path = path.strip()
-            print ("Testing Audio : ", path)
-            sr,audio = read(source + path)
-            vector   = extract_features(audio,sr)
-            log_likelihood = np.zeros(len(models))
+        # Read the test directory and get the list of test audio files
+        for file in files:
+                total_sample += 1.0
+                path = path.strip()
+                print ("Testing Audio : ", speaker, file)
+                sr,audio = read(os.path.join(path,speaker,file))
+                vector   = extract_features(audio,sr)
+                log_likelihood = np.zeros(len(models))
 
-            for i in range(len(models)):
-                gmm = models[i]
-                scores = np.array(gmm.score(vector))
-                log_likelihood[i] = scores.sum()
+                for i in range(len(models)):
+                    gmm = models[i]
+                    scores = np.array(gmm.score(vector))
+                    log_likelihood[i] = scores.sum()
 
-            winner = np.argmax(log_likelihood)
-            print ("\tdetected as - ", speakers[winner])
+                winner = np.argmax(log_likelihood)
+                print ("\tdetected as - ", speakers[winner])
 
-            checker_name = path.split("_")[0]
-            if speakers[winner] != checker_name:
-                error += 1
-                time.sleep(1.0)
+                checker_name = speaker#path.split("_")[0]
 
-    print (error, total_sample)
+                if speakers[winner] != checker_name:
+                    error += 1
+                    time.sleep(1.0)
+
+    print(error,"errors out of",total_sample, "samples.")
     accuracy = ((total_sample - error) / total_sample) * 100
-
     print ("The Accuracy Percentage for the current testing Performance with MFCC + GMM is : ", accuracy, "%")
-
-
-print ("Hurrey ! Speaker identified. Mission Accomplished Successfully. "
-)
